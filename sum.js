0ba644331ec91983693e315454419a72041526a5
ashwinip1503 <ashwinipadwal0315@gmail.com>
2026-03-19 19:33:08 +0530

    sum js

---
+++ sum.js
@@ -0,0 +1,12 @@
+// write a function that takes two numbers and returns their sum
+
+function sum(a,b){
+    return a+b
+}
+ 
+console.log(sum(1,2))
+console.log(sum(123,234))
+console.log(sum(-1,2))
+console.log(sum(-1,-2))
+console.log(sum(1.5,2.5))
+
